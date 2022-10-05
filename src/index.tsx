ddc475309d313d6b8cfc2974cc0d57e6c6f2a941
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-10-05 14:59:07 +0300

    wadwa

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ReactNotifications } from "react-notifications-component";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ReactNotifications />
     <App />
   </React.StrictMode>
 );
